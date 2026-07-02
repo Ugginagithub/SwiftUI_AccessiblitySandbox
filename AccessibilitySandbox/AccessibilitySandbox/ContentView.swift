187bc9d5da48b72c2972f7f550dec4cdab922a64
Tarun Uggina <tarun.uggina@gmail.com>
2026-07-02 17:48:46 +0530

    Learn about accessiblity more.
    Learn about accessiblity more.

--- AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
+++ AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    let pictures = [
+        "ales-krivec-15949",
+        "galina-n-189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096"
+    ]
+    
+    let labels = [
+        "Tulips",
+        "Frozen tree buds",
+        "Sunflowers",
+        "Fireworks"
+    ]
+    
+    @State private var selectedPictures = Int.random(in: 0...3)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        Button {
+            selectedPictures = Int.random(in: 0...3)
+        } label: {
+            Image(pictures[selectedPictures])
+                .resizable()
+                .scaledToFit()
         }
-        .padding()
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel(labels[selectedPictures])
+        
+        //MARK: Hiding and grouping accessiblity data
+//        Image(decorative: "character")
     }
 }
 
